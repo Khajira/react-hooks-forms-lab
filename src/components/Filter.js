b5419bf569170e0cf98e52922a8b466148c1b299
Benard Khajira <benardkhajira@gmail.com>
2024-06-11 23:11:37 +0300

    complete lab

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Item from "./Item";
 
 function Filter({ onCategoryChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
       </select>
+    </div>
+  );
+}
+
+function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
+  return (
+    <div className="ShoppingList">
+      <Filter onCategoryChange={handleCategoryChange} />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
     </div>
   );
 }
